4dbd5f4ae72437e3621ffdd889ce3ba20a3b8c48
rexbordz <41312558+rexbordz@users.noreply.github.com>
2026-09-01 08:34:54 +0800

    Made the tiktok subscriber badge brighter

--- utils/tiktok-emotes-and-badges.js
+++ utils/tiktok-emotes-and-badges.js
@@ -163,7 +163,7 @@ const tikTokBadgeData = {
     ],
 
     fanSubscriber: {
-        border: "#e97942",
+        border: "#ffb878",
         tiers: [
             { min: 1,  icon: "fans_badge_icon_lv1_v4.png~tplv-obj.image",  color: "rgba(255, 94, 58, .5)" },
             { min: 10, icon: "fans_badge_icon_lv10_v4.png~tplv-obj.image", color: "rgba(255, 94, 58, .5)" },
